9cf311d0a2015172d67a0b6a9967c30387285472
Logan Farci <farci.logan@gmail.com>
2019-03-23 14:20:25 +0100

    Adjacency test cases that were in MazeTest.cpp are now in MazeAdjacencyTest

--- tests/mazeadjacencytest.cpp
+++ tests/mazeadjacencytest.cpp
@@ -7,7 +7,22 @@
 
 using namespace labyrinth;
 
-TEST_CASE("Two position linked by a path are adjacent")
+TEST_CASE("Two maze cards linked by a direct path should be neighbors")
+{
+
+}
+
+TEST_CASE("Two maze cards that are not directly linked should not be neighbors")
+{
+
+}
+
+TEST_CASE("areNeighbors throws an exception if the first position is invalid")
+{
+
+}
+
+TEST_CASE("areNeighbors throws an exception if the second position is invalid")
 {
 
 }
