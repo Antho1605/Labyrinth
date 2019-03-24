5bc95251758bc99bc1b4118c057a00506665f013
Logan Farci <farci.logan@gmail.com>
2019-03-24 13:08:24 +0100

    Working on the maze initialization test

--- tests/mazetest.cpp
+++ tests/mazetest.cpp
@@ -7,3 +7,17 @@
 
 using namespace labyrinth;
 
+TEST_CASE("The maze is initialized as expected")
+{
+    Maze m;
+    m.initialize();
+    for (unsigned row = 0; row < m.SIZE; ++row) {
+        for (unsigned column = 0; column < m.SIZE; ++column) {
+            if (row % 2 == 0 && column % 2 == 0) {
+                CHECK(!m.getCardAt(MazePosition{row, column}).isMovable());
+            } else {
+                CHECK(!m.getCardAt(MazePosition{row, column}).isMovable());
+            }
+        }
+    }
+}
